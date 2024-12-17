885bd1286d78ea1b817b734225aba13d04b9177a
theoriginalAdamTay <117193548+theoriginalAdamTay@users.noreply.github.com>
2024-12-17 22:37:11 +1100

    Create DelistAndRedefine.cs
    Added DelistAndRedefine C# method for defining list of integers and allowing filling in blanks when elements are deleted.

---
+++ DelistAndRedefine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArrayBlankFiller {
+	public ArrayBlankFiller() {
+		public static void Main(String[] args) { // Main method
+			List<int> numberList = new List<int>(); // Define list of integers
+		}
+
+		public void DelistAndRedefine(List<int> list, int delistElement) { // Method for redefining list indices
+			list.Remove(delistElement); // Remove element from list specified in arguments
+			for (int i = 0; i < list.Count; i++) { // For each list index execute code below
+				list[i] = i + 1; // Redefine list indices
+			}
+		}
+	}
+}
